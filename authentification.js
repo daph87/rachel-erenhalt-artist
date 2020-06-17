abe1ba0db4898f7249628a42d7c28f599f85b7aa
Daphne Levy <87daphnelevy@gmail.com>
2020-06-17 17:47:55 +0300

    new-font

---
+++ authentification.js
@@ -0,0 +1,11 @@
+let secret_obj = {
+    mail: 'zoomgallerytlv@gmail.com', 
+    api_key: 'fef411747bc04415cd9d84d345a69efc-0afbfc6c-bb976362',
+    domain: 'sandbox872bf1f171bc45448938885a82481807.mailgun.org'
+
+    
+}
+
+module.exports = {
+    secret_obj
+}
